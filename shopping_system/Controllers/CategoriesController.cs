c1a6d2c626e03a2abcf832f7118f68bd0c73d5c1
yourma-ko <maximbakhturazov@gmail.com>
2024-03-19 23:53:19 +0500

    added CRUD operations for categories

--- shopping_system/Controllers/CategoriesController.cs
+++ shopping_system/Controllers/CategoriesController.cs
@@ -7,14 +7,49 @@ namespace shopping_system.Controllers
     {
         public IActionResult Index()
         {
-            var categories = CategoriesReopsitory.GetCategories();
+            var categories = CategoriesRepository.GetCategories();
             return View(categories);
         }
         public IActionResult Edit(int? id)
         {
-            var category = new Category { CategoryId = id.HasValue ? id.Value : 0 };
+            ViewBag.Action = "edit";
+            var category = CategoriesRepository.GetCategoryById(id.HasValue?id.Value: 0);
+            
             return View(category);
 
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.UpdateCategory(category.CategoryId, category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+
+            
+        }
+        public IActionResult Add()
+        {
+            ViewBag.Action = "add";
+            return View();
+        }
+        [HttpPost] 
+        public IActionResult Add(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                CategoriesRepository.AddCategory(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+
+        }
+        public IActionResult Delete(int categoryId)
+        {
+            CategoriesRepository.DeleteCategory(categoryId);
+            return RedirectToAction(nameof(Index));
         }
     }
 
--- shopping_system/Models/Category.cs
+++ shopping_system/Models/Category.cs
@@ -1,9 +1,12 @@
-﻿namespace shopping_system.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace shopping_system.Models
 {
     public class Category
     {
         public int CategoryId { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
+        public string? Description { get; set; } = string.Empty;
     }
 }
